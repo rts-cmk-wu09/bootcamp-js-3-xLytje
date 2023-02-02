a7e0db1e2b69d571525cdcc9de5377935a36e6a5
xLytje <rasmuslytje@gmail.com>
2023-02-02 10:57:08 +0100

    first attemp at JS

---
+++ nav.js
@@ -0,0 +1,35 @@
+//let link = document.querySelectorAll("nav ul a")
+//
+//link.forEach((element, index) => {
+//    element.addEventListener("click", (e)=>{
+//        e.preventDefault()
+//
+//    })
+//})
+
+let content = document.querySelector(".main")
+
+let pages = {
+    "#page1": "page1.html",
+    "#page2": "page2.html",
+    "#page3": "page3.html",
+}
+
+window.addEventListener("hashchange", () => {
+    loadPage(window.location.hash)
+})  
+
+function loadPage(pageHash) {
+    fetch(pages[pageHash])
+        .then((response) => response.text())
+        .then((data) =>{
+            content.innerHTML = data
+        })
+}
+
+if(!window.location.hash){
+    loadPage("#page1")
+}
+else{
+    loadPage(window.location.hash)
+}
